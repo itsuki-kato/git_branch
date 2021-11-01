0dd895fa3a3d89cb98109a5ee6ebb91d88c7bf27
itsuki <ituki.larc@gmail.com>
2021-11-01 14:27:35 +0900

    first commit

---
+++ barnch.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>ブランチ練習</h1>
+    <p>mainブランチのコミット</p>
+</body>
+</html>
